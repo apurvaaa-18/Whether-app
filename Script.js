d8839e89e43abe61e139c85667f08f8969ac6c40
apurvaaa-18 <apurvapatil1814@gmail.com>
2026-08-11 21:20:01 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+let cityInput = document.queryselector("input");
+let whetherbutton = document.queryselector("button");
+weatherButton.addEventListener("click", function() {
+    let city = city input.value;
+let weather = document.querySelector("#weather");
+
+    weather.innerText = "You searched for: " + city;
+  
+});
